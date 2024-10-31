9c3594bf52f7dedd5d827ced65cffff488e9630d
SAGNIK_MITRA <sagnikmitra008@gmail.com>
2024-11-01 00:45:30 +0530

    added all the files

---
+++ src/monster/Monster.java
@@ -0,0 +1,18 @@
+package src.monster;
+
+import src.entity.Entity;
+import src.main.GamePanel;
+
+public class Monster extends Entity {
+  public Monster(GamePanel gp) {
+      super(gp);
+      //this.worldX = startX;
+      //this.worldY = startY;
+      // Initialize other monster attributes (e.g., health, speed)
+  }
+  
+  public void update() {
+      // Add movement or AI behavior
+  }
+}
+
---
+++ src/monster/Monster_blue_slime.java
@@ -0,0 +1,19 @@
+package src.monster;
+
+// import src.entity.Entity;
+import src.main.GamePanel;
+
+public class Monster_blue_slime extends Monster{
+
+  public Monster_blue_slime(GamePanel gp){
+    
+    super(gp);
+
+    // name = "Green Slime";
+    speed = 1;
+    // maxLife = 4;
+    
+  }
+
+  
+}
